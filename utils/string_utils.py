7dd49ed3656c4345abebbba6d7647933ce8d8881
pavel schudel <pavel1860@gmail.com>
2024-09-17 12:38:32 +0300

    added ability for passing messages as views, added jinja formatter for messages, added tests for jinja formatter

--- utils/string_utils.py
+++ utils/string_utils.py
@@ -1,5 +1,5 @@
 import re
-
+from jinja2 import Environment
 
 def render_tabs(num: int):
     return "\t" * num
@@ -14,3 +14,19 @@ def convert_camel_to_snake(name):
     """Convert CamelCase to snake_case"""
     s1 = re.sub('([a-z0-9])([A-Z])', r'\1_\2', name)
     return s1.lower()
+
+
+
+
+class SafeJinjaFormatter:
+    def format(self, template_string, **kwargs):
+        # Create a Jinja2 environment
+        env = Environment()
+
+        # Render the template with the given kwargs
+        # Use the env.from_string method to create a template from the provided string
+        template = env.from_string(template_string)
+        return template.render(**kwargs)
+    
+    def __call__(self, template_string, **kwargs):
+        return self.format(template_string, **kwargs)
