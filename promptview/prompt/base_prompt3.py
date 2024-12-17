6751038efeb5a8f85dbb34f7922b2ab5dc6062ea
pavel schudel <pavel1860@gmail.com>
2024-12-17 10:45:10 +0200

    added dependency injection

--- promptview/prompt/base_prompt3.py
+++ promptview/prompt/base_prompt3.py
@@ -10,6 +10,7 @@ from promptview.llms.messages import AIMessage, BaseMessage, HumanMessage
 from promptview.llms.openai_llm import OpenAiLLM
 from promptview.llms.tracer import Tracer
 from promptview.llms.utils.action_manager import Actions
+from promptview.prompt.depends import Depends, DependsContainer, resolve_dependency
 from promptview.prompt.mvc import ViewBlock, create_view_block
 from promptview.state.context import Context
 from promptview.utils.function_utils import call_function
@@ -20,11 +21,25 @@ P = ParamSpec('P')
 R = TypeVar('R')
 
 
+
+
 class Prompt(Generic[P, R]):
     _name: str
     _complete: Callable[P, R]
-    # async def complete(self, *args: P.args, **kwargs: P.kwargs) -> T:
-    #     raise NotImplementedError()
+    
+    
+    async def _call_with_dependencies(self, *args, **kwargs):
+        signature = inspect.signature(self._complete)
+        injection_kwargs = {}
+        for param_name, param in signature.parameters.items():
+            default_val = param.default
+            if isinstance(default_val, DependsContainer):
+                dependency_func = default_val.dependency
+                resolved_val = await resolve_dependency(dependency_func,  *args, **kwargs)
+                injection_kwargs[param_name] = resolved_val
+                
+        res = await call_function(self._complete, *args, **kwargs, **injection_kwargs)
+        return res
     
     
     async def __call__(self, *args: P.args, **kwargs: P.kwargs) -> R:
@@ -36,7 +51,8 @@ class Prompt(Generic[P, R]):
                 # session_id=context.session_id,
                 # tracer_run=tracer_run
             ) as run:
-            res = await self._complete(*args, **kwargs)
+            inspect.signature(self._complete).bind(*args, **kwargs)
+            res = await self._call_with_dependencies(*args, **kwargs)
             run.add_outputs({"response":res})
             return res
     
---
+++ promptview/prompt/depends.py
@@ -0,0 +1,97 @@
+
+
+
+
+import inspect
+from typing import Annotated, Callable, Generic, ParamSpec, TypeVar
+from typing_extensions import Doc
+
+from aiohttp_retry import Any, Optional
+from promptview.utils.function_utils import call_function
+
+
+# P = ParamSpec('P')
+# R = TypeVar('R')
+
+# class Depends(Generic[P, R]):
+#     def __init__(self, dependency: Callable[P, R]):
+#         self.dependency = dependency
+
+# class Depends:
+#     def __init__(
+#         self, dependency: Optional[Callable[..., Any]] = None, *, use_cache: bool = True
+#     ):
+#         self.dependency = dependency
+#         self.use_cache = use_cache
+
+#     def __repr__(self) -> str:
+#         attr = getattr(self.dependency, "__name__", type(self.dependency).__name__)
+#         cache = "" if self.use_cache else ", use_cache=False"
+#         return f"{self.__class__.__name__}({attr}{cache})"
+
+
+
+class DependsContainer:
+    def __init__(
+        self, dependency: Optional[Callable[..., Any]] = None, *, use_cache: bool = True
+    ):
+        self.dependency = dependency
+        self.use_cache = use_cache
+
+    def __repr__(self) -> str:
+        attr = getattr(self.dependency, "__name__", type(self.dependency).__name__)
+        cache = "" if self.use_cache else ", use_cache=False"
+        return f"{self.__class__.__name__}({attr}{cache})"
+
+
+def Depends(  # noqa: N802
+    dependency: Annotated[
+        Optional[Callable[..., Any]],
+        Doc(
+            """
+            A "dependable" callable (like a function).
+
+            Don't call it directly, Chatboard will call it for you, just pass the object
+            directly.
+            """
+        ),
+    ] = None,
+    *,
+    use_cache: Annotated[
+        bool,
+        Doc(
+            """
+            By default, after a dependency is called the first time in a request, if
+            the dependency is declared again for the rest of the request (for example
+            if the dependency is needed by several dependencies), the value will be
+            re-used for the rest of the request.
+
+            Set `use_cache` to `False` to disable this behavior and ensure the
+            dependency is called again (if declared more than once) in the same request.
+            """
+        ),
+    ] = True,
+) -> Any:
+    return DependsContainer(dependency=dependency, use_cache=use_cache)
+
+
+
+
+async def resolve_dependency(dependency_func, *args, **kwargs):
+    """
+    Recursively resolve the dependencies of `dependency_func` by inspecting
+    its signature. Calls sub-dependencies first, then calls `dependency_func`.
+    """
+    signature = inspect.signature(dependency_func)
+    dep_kwargs = {}
+    
+    for param_name, param in signature.parameters.items():
+        default_val = param.default
+        
+        # If the default is a Depends(...) object, recursively resolve it
+        if isinstance(default_val, DependsContainer):
+            sub_dependency_func = default_val.dependency
+            dep_kwargs[param_name] = resolve_dependency(sub_dependency_func)
+    
+    # Once all sub-dependencies are resolved, call the function
+    return await call_function(dependency_func, *args, **kwargs, **dep_kwargs)
