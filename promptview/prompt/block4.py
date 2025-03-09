37c5a3e7851ac871a31015867e75721f522bc2f9
pavel schudel <pavel1860@gmail.com>
2025-03-09 15:43:51 +0200

    fixed block4.

--- promptview/prompt/block4.py
+++ promptview/prompt/block4.py
@@ -9,16 +9,16 @@ from typing import Any, List, Type
 
 class TagRegistry:
     
-    tags: defaultdict[str, list["Block"]]
+    tags: defaultdict[str, list["BaseBlock"]]
     
     def __init__(self):
         self.tags = defaultdict(list)
 
-    def add(self, block: "Block"):
+    def add(self, block: "BaseBlock"):
         for tag in block.tags:
             self.tags[tag].append(block)        
     
-    def get(self, tag: str | list[str]) -> list["Block"]:
+    def get(self, tag: str | list[str]) -> list["BaseBlock"]:
         if isinstance(tag, str):
             return self.tags[tag]
         else:
@@ -38,29 +38,29 @@ class TagRegistry:
      
         
         
-class Block:
+class BaseBlock:
     
     tags: list[str]
-    items: list["Block"]
+    items: list["BaseBlock"]
     
     
-    def __init__(self, tags: list[str] | None = None, depth: int = 1):
+    def __init__(self, tags: list[str] | None = None, depth: int = 0):
         self.tags = tags or []
         self.items = []
-        self.depth = depth
+        self.depth = depth or 0
         
         
-    def append(self, item: "Block | Any"):
+    def append(self, item: "BaseBlock | Any"):
         # if not isinstance(item, Block):
             # item = self._build_instance(item)
         self.items.append(item)
         return item
         
-    def __add__(self, other: "Block | Any"):
+    def __add__(self, other: "BaseBlock | Any"):
         item = self.append(other)
         return item
     
-    def __iadd__(self, other: "Block | Any"):
+    def __iadd__(self, other: "BaseBlock | Any"):
         self.append(other)
         return self
        
@@ -80,12 +80,9 @@ class Block:
     def render(self):
         raise NotImplementedError("Subclasses must implement this method")
     
-    
     @abstractmethod
     def parse(self, text: str):
         raise NotImplementedError("Subclasses must implement this method")
-        
-        
         
     def __enter__(self):
         return self
@@ -93,12 +90,6 @@ class Block:
     def __exit__(self, exc_type, exc_value, traceback):
         pass
         
-        
-
-
-
-
-
 
 
     
@@ -108,8 +99,8 @@ class Block:
     
 class BlockContext(object):
     
-    _block_type_registry: dict[Type, Type[Block]]
-    _ctx_stack: "List[Block]"
+    _block_type_registry: dict[Type, Type[BaseBlock]]
+    _ctx_stack: "List[BaseBlock]"
     
     
     def __new__(cls, *args, **kwargs):
@@ -121,38 +112,38 @@ class BlockContext(object):
     def __init__(
         self,
         value: Any | None = None,
-        *args,
         **kwargs,
         # ctx_stack: "List[Block] | None" = None, 
     ):
-        self._block = block
+        # self._block = block
         # self._staged_inst = self._build_instance(value, *args, **kwargs)
         self._ctx_stack = []
-        inst = self._build_instance(value, 1, *args, **kwargs)
-        self._ctx_stack.append(inst)
+        inst = self._build_instance(value, **kwargs)
+        self._main_inst = inst
         # self._ctx_stack.append(inst)
     
     
-    def __call__(self, value: Any, *args, **kwargs):
-        if self._staged_inst is not None:
-            self._append(self._staged_inst)
-        # self._staged_inst = self._build_instance(value, *args, **kwargs)
-        inst = self._build_instance(value, depth=len(self._ctx_stack), *args, **kwargs)
-        self._append(inst)
+    def __call__(self, value: Any, **kwargs):       
+        self._append(value, **kwargs)
         return self
     
+    def _append(self, value: Any, **kwargs):
+        inst = self._build_instance(value, **kwargs)        
+        self._ctx_stack[-1].append(inst)
+        return inst
+
+    
     @property
-    def _staged_inst(self):
-        curr_blk = self._ctx_stack[-1]
-        if curr_blk.items:
-            return curr_blk.items[-1]
-        return curr_blk
+    def top(self):
+        if not self._ctx_stack:
+            raise ValueError("No context stack")
+        return self._ctx_stack[-1]
         
-    def __enter__(self):
-        # if self._staged_inst is None:
-        #     raise ValueError("No instance to append")
-        self._ctx_stack.append(self._staged_inst)
-        # self._staged_inst = None
+    def __enter__(self):        
+        if not self._ctx_stack:
+            self._ctx_stack.append(self._main_inst)
+        else:
+            self._ctx_stack.append(self.top)
         return self
     
     def __exit__(self, exc_type, exc_value, traceback):
@@ -161,34 +152,31 @@ class BlockContext(object):
 
         
     @classmethod
-    def register(cls, typ: Type, block_type: Type[Block]):
+    def register(cls, typ: Type, block_type: Type[BaseBlock]):
         if not hasattr(cls, "_block_type_registry"):
             cls._block_type_registry = {}
         cls._block_type_registry[typ] = block_type    
     
         
-    def _build_instance(self, value: Any, depth: int, *args, **kwargs):
-        kwargs["depth"] = depth
-        return self.__class__._block_type_registry[type(value)](value, *args, **kwargs)
+    def _build_instance(self, value: Any, **kwargs):
+        # if "depth" not in kwargs:
+            # kwargs["depth"] = depth
+        inst = self.__class__._block_type_registry[type(value)](value, **kwargs)
+        inst.depth = len(self._ctx_stack) + 1
+        return inst
     
-    def _append(self, value: Any):
-        self._ctx_stack[-1].append(value)
     
     def __add__(self, value: Any):
-        inst = self._build_instance(value, depth=len(self._ctx_stack))
-        self._append(inst)
+        inst = self._append(value)
         return inst
     
     def __iadd__(self, value: Any):
-        inst = self._build_instance(value, depth=len(self._ctx_stack))
-        self._append(inst)
+        self._append(value)
         return self
     
     def render(self):
         return self._ctx_stack[-1].render()
     
-    # def __getattr__(self, item):
-        # return getattr(self.block, item)
     
     
     
