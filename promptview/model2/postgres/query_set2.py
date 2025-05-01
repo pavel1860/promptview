3b8bb31e0a5d735cd64de3713e3420eb8eb71595
pavel schudel <pavel1860@gmail.com>
2025-05-01 23:36:53 +0300

    removed the subquery

--- promptview/model2/postgres/query_set2.py
+++ promptview/model2/postgres/query_set2.py
@@ -14,7 +14,7 @@ from typing_extensions import TypeVar
 from promptview.model2.base_namespace import Namespace
 from promptview.model2.model import Model
 from promptview.model2.postgres.fields_query import PgFieldInfo
-from promptview.model2.postgres.query_builders2 import Coalesce, Filter, JsonAgg, JsonBuild, JsonNestedQuery, JsonSubNestedQuery, SelectQuery, TableName, Where
+from promptview.model2.postgres.query_builders2 import Coalesce, Filter, JsonAgg, JsonBuild, JsonNestedQuery, JsonSubNestedQuery, Query, SelectQuery, TableName, Where
 from promptview.model2.query_filters import QueryProxy
 from promptview.utils.db_connections import PGConnectionManager
 
@@ -45,16 +45,6 @@ class BaseQuerySet(Generic[MODEL, ORIG_MODEL, QUERY_TYPE]):
     def namespace(self) -> Namespace[MODEL, PgFieldInfo]:
         return self.model_class.get_namespace()
         
-        
-    # def init_query(self, model_class: Type[MODEL]) -> QUERY_TYPE:
-    #     raise NotImplementedError("Subclasses must implement this method")
-        
-    # def select(self, *args: str, **kwargs: str) -> "SelectQuerySet[MODEL]":
-    #     raise NotImplementedError("Subclasses must implement this method")
-    
-    # def alias(self, alias: str) -> "SelectQuerySet[MODEL]":
-    #     raise NotImplementedError("Subclasses must implement this method")
-    
     def render(self, new_line: bool = True) -> str:
         return self.query.render(new_line)
         
@@ -78,19 +68,45 @@ class BaseQuerySet(Generic[MODEL, ORIG_MODEL, QUERY_TYPE]):
     
     
     
-class SelectQuerySet(BaseQuerySet[MODEL, MODEL, SelectQuery]):
+class SelectQuerySet(Generic[MODEL]):
+    
+    model_class: Type[MODEL]
     query: SelectQuery
-    parent_query: SelectQuery | None = None
-    main_query: SelectQuery | None = None
+    query_stack: list[Query]
+    
     
     
     def __init__(self, model_class: Type[MODEL]):
         # self.model_class = model_class
-        query = SelectQuery(
+        self.model_class = model_class
+        self.query = SelectQuery(
             select="*",
             from_table=TableName(model_class.get_namespace().table_name)
         )
-        super().__init__(model_class, query, model_class, self)
+        self.query_stack = [self.query]
+        
+    @property
+    def namespace(self) -> Namespace[MODEL, PgFieldInfo]:
+        return self.model_class.get_namespace()
+     
+    @property
+    def curr_query(self):
+        return self.query_stack[-1]
+         
+    
+    async def execute_sql(self, sql: str, *values: Any) -> List[Any]:
+        return await PGConnectionManager.fetch(sql, *values)
+    
+    def pack_record(self, data: Any) -> Any:
+        """Pack the record for the model"""
+        return self.model_class.get_namespace().pack_record(data)
+
+    
+    async def execute(self) -> List[MODEL]:
+        sql = self.render()
+        results = await self.execute_sql(sql)
+        return [self.model_class(**self.pack_record(dict(result))) for result in results] 
+
         
     def select(self, *args: str, **kwargs: str) -> "SelectQuerySet[MODEL]":
         if len(args) and args[0] == "*":
@@ -99,83 +115,40 @@ class SelectQuerySet(BaseQuerySet[MODEL, MODEL, SelectQuery]):
             fields = list(args) 
             if kwargs:
                 fields += [(v, k) for k, v in kwargs.items()]
-        self.query._select = fields
+        self.curr_query._select = fields
         return self
     
     def alias(self, alias: str) -> "SelectQuerySet[MODEL]":
-        self.query.alias(alias)
+        self.curr_query.alias(alias)
         return self
     
-    def join(self, model: Type[Model]) -> "NestedQuerySet[MODEL, Self, JsonNestedQuery]":        
+    def join(self, model: Type[Model]) -> "SelectQuerySet[MODEL]":        
         relation = self.namespace.get_relation_by_type(model)
         fields = [f.name for f in model.iter_fields()]
         foreign_table = TableName(model.get_namespace().table_name)
         query = JsonNestedQuery(
             select=fields,
             from_table=foreign_table,
-            parent_table=self.query.table_name,
+            parent_table=self.curr_query.table_name,
             primary_key=relation.primary_key,
         )
-        nested_query = NestedQuerySet(model, query, self.orig_model, self.orig_query_set)
-        self.query.add_select((nested_query, relation.name))
-        self.query.join(self.query.table_name, relation.primary_key, foreign_table, relation.foreign_key)
-        self.query.group_by(relation.primary_key)
-        return nested_query
+        # nested_query = NestedQuerySet(model, query, self.orig_model, self.orig_query_set)
+        
+        self.curr_query.add_select((query, relation.name))
+        self.curr_query.join(self.curr_query.table_name, relation.primary_key, foreign_table, relation.foreign_key)
+        self.curr_query.group_by(relation.primary_key)
+        self.query_stack.append(query)
+        return self
     
 
     def where(self, filter_fn: Callable[[MODEL], bool] | None = None, **kwargs) -> "SelectQuerySet[MODEL]":
         """Filter the query"""           
         if filter_fn is not None:
             proxy = QueryProxy[MODEL, PgFieldInfo](self.model_class, self.namespace)
-            self.query.where(filter_fn(proxy))
+            self.curr_query.where(filter_fn(proxy))
         return self
     
     def render(self, new_line: bool = True) -> str:
         return self.query.render(new_line)
     
     
-    
-    
-
-    
-    
-class NestedQuerySet(BaseQuerySet[MODEL, ORIG_MODEL,  QUERY_TYPE]):
-        
-    def __init__(self, model_class: Type[MODEL], query: QUERY_TYPE, orig_model: ORIG_MODEL, parent_query: "BaseQuerySet | None" = None):
-        super().__init__(model_class, query, orig_model, parent_query)
-    
-    
-    def select(self, *args: str, **kwargs: str) -> "SelectQuerySet[MODEL]":
-        fields = list(args) 
-        if kwargs:
-            fields += [(v, k) for k, v in kwargs.items()]
-        self.query._select = fields
-        return self  
-    
-    def join(self, model: Type[Model]) -> "NestedQuerySet[MODEL, Self, QUERY_TYPE]":
-        relation = self.namespace.get_relation_by_type(model)
-        fields = [f.name for f in model.iter_fields()]
-        foreign_table = TableName(model.get_namespace().table_name)
-        query = JsonSubNestedQuery(
-            select=fields,
-            from_table=foreign_table,
-            parent_table=self.query.table_name,
-            foreign_key=relation.foreign_key,
-        )
-        nested_query = NestedQuerySet(model, query, self.orig_model, self.orig_query_set)
-        self.query.add_select((nested_query, relation.name))
-        self.orig_query_set.query.join(self.query.table_name, relation.primary_key, foreign_table, relation.foreign_key)
-        return nested_query
-
-
-    def alias(self, alias: str) -> "NestedQuerySet[MODEL, ORIG_MODEL, QUERY_TYPE]":
-        self.query.alias(alias)
-        return self
-
-    
-    def where(self, filter_fn: Callable[[MODEL], bool] | None = None, **kwargs) -> "NestedQuerySet[MODEL, ORIG_MODEL, QUERY_TYPE]":
-        """Filter the query"""           
-        if filter_fn is not None:
-            proxy = QueryProxy[MODEL, PgFieldInfo](self.model_class, self.namespace)
-            self.query.where(filter_fn(proxy))
-        return self
